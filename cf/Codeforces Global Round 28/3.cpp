0e3824295e4564346d7843b143eb5e038fcdd2a9
Kristina1Q <3463207315@qq.com>
2024-12-23 20:03:01 +0800

    cf

--- cf/Codeforces Global Round 28/3.cpp
+++ cf/Codeforces Global Round 28/3.cpp
@@ -2,8 +2,47 @@
 #include <bits/stdc++.h>
 #define LL long long
 using namespace std;
+void solve()
+{
+    string arr;
+    cin>>arr;
+    int l2=1,r2=arr.size(),l1=1,r1=1;
+    int i,j,q;
+    for(i=0;i<arr.size();i++)
+    {
+        if(arr[i]=='0')
+        {
+            l1=i,r1=arr.size()-i;
+            int max1=0,ans;
+            for(j=i-1;j>=0;--j){
+                ans=0;
+                for(q=j;q<=arr.size()-(i-j)-1;++q)
+                {
+                    if(arr[q+(i-j)]!=arr[q])ans++;
+                    else break;
+                }
+                if(ans>max1){
+                l1=j+1;
+                r1=arr.size()-(i-j);
+                max1=ans;
+                }
+            }
+            // cout<<max1<<'\n';
+            cout<<l1<<' '<<r1<<' '<<l2<<' '<<r2<<'\n';
+            return;
+        }
+    }
+    cout<<l1<<' '<<r1<<' '<<l2<<' '<<r2<<'\n';
+    return;
+}
 int main()
 {
-
+    ios::sync_with_stdio(0),cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
     return 0;
 }
--- cf/exersise/10.cpp
+++ cf/exersise/10.cpp
@@ -4,7 +4,37 @@
 using namespace std;
 void solve()
 {
-    
+    LL n;
+    cin>>n;
+    if((n&1)==0){
+        // cout<<"not\n";
+        LL r=0,c=0;
+        for(LL i=1;i<=n;i++){
+            if((n|i)>=r){
+                c=i;
+                r=n|i;
+            }
+        }
+        // cout<<r<<' '<<c<<'\n';
+        cout<<r<<'\n';
+        cout<<2<<' ';
+        for(LL i=4;i<n;++i)
+        {
+            if(i!=c&&i!=c-1)
+            cout<<i<<' ';
+        }
+        cout<<1<<' '<<3<<' '<<c-1<<' '<<c<<' '<<n<<'\n';
+    }
+    else
+    {
+        cout<<n<<'\n';
+        cout<<2<<' ';
+        for(LL i=4;i<n-1;++i)
+        {
+            cout<<i<<' ';
+        }
+        cout<<1<<' '<<3<<' '<<n-1<<' '<<n<<'\n';
+    }
 }
 int main()
 {
